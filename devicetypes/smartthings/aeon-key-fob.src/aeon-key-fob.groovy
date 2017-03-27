876e554c5b053ed83da51ced5264f9e163c3400b
Richard Walker <richard@walkerall.com>
2017-03-27 16:00:35 +0100

    aaa

--- devicetypes/smartthings/aeon-key-fob.src/aeon-key-fob.groovy
+++ devicetypes/smartthings/aeon-key-fob.src/aeon-key-fob.groovy
@@ -10,6 +10,26 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * ===== Nodon Octan =====
+ * 	
+ *	Modified for NodOn Octan Remote (CRC-3-1-0x) by Richard_Woodward
+ *
+ *	Octan supports 4 actions per button	Pushed
+ *										Double Tap
+ *										Held
+ *										Hold Released
+ *
+ *	v1.01 Updated to support all 16 actions:
+ *  
+ *  As Smartlighting can only support type "Pushed" or "Hold" events the extra 4 functions have been mapped to 
+ *  fictional buttons 5 through 8.
+ *
+ *  Button 1 double Tap is mapped to   "Button 5 Pushed" in smartlighting
+ *  Button 1 Hold Release is mapped to "Button 5 Hold"   in smarlighting
+ *  .
+ *  Button 4 double Tap is mapped to   "Button 8 Pushed" in Smartlighting
+ *  Button 4 Hold Rlease is mapped to  "Button 8 Hold"   in Smartlighting
+ *
  */
 metadata {
 	definition (name: "Aeon Key Fob", namespace: "smartthings", author: "SmartThings") {
@@ -59,11 +79,12 @@ def parse(String description) {
 	if (description.startsWith("Err")) {
 	    results = createEvent(descriptionText:description, displayed:true)
 	} else {
-		def cmd = zwave.parse(description, [0x2B: 1, 0x80: 1, 0x84: 1])
+		def cmd = zwave.parse(description, [0x80: 1, 0x84: 1])
+        //log.debug("cmd=$cmd")
 		if(cmd) results += zwaveEvent(cmd)
 		if(!results) results = [ descriptionText: cmd, displayed: false ]
 	}
-	// log.debug("Parsed '$description' to $results")
+	//log.debug("Parsed '$description' to $results")
 	return results
 }
 
@@ -73,6 +94,7 @@ def zwaveEvent(physicalgraph.zwave.commands.wakeupv1.WakeUpNotification cmd) {
 	def prevBattery = device.currentState("battery")
 	if (!prevBattery || (new Date().time - prevBattery.date.time)/60000 >= 60 * 53) {
 		results << response(zwave.batteryV1.batteryGet().format())
+        log.debug("  read battery")
 	}
 	results += configurationCmds().collect{ response(it) }
 	results << response(zwave.wakeUpV1.wakeUpNoMoreInformation().format())
@@ -81,18 +103,30 @@ def zwaveEvent(physicalgraph.zwave.commands.wakeupv1.WakeUpNotification cmd) {
 
 def buttonEvent(button, held) {
 	button = button as Integer
-	if (held) {
+    held = held as Integer
+    //log.debug("button=$button Held=$held")
+	if( held == 1) {
+    	def ebutton = button +4
+    	createEvent(name: "button", value: "held", data: [buttonNumber: ebutton], descriptionText: "$device.displayName button $ebutton was pushed", isStateChange: true)
+    }
+    else if (held == 2) {
 		createEvent(name: "button", value: "held", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was held", isStateChange: true)
-	} else {
+    }
+    else if (held==3) {
+    	def ebutton = button +4
+    	createEvent(name: "button", value: "pushed", data: [buttonNumber: ebutton], descriptionText: "$device.displayName button $ebutton was pushed", isStateChange: true)
+    }
+    else if (held == 0) {
 		createEvent(name: "button", value: "pushed", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was pushed", isStateChange: true)
 	}
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.sceneactivationv1.SceneActivationSet cmd) {
-	Integer button = ((cmd.sceneId + 1) / 2) as Integer
-	Boolean held = !(cmd.sceneId % 2)
+    Integer button = (cmd.sceneId / 10) as Integer
+    Integer held = cmd.sceneId - (button*10) as Integer
 	buttonEvent(button, held)
 }
+
 
 def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
 	def map = [ name: "battery", unit: "%" ]
@@ -110,13 +144,19 @@ def zwaveEvent(physicalgraph.zwave.Command cmd) {
 }
 
 def configurationCmds() {
-	[ zwave.configurationV1.configurationSet(parameterNumber: 250, scaledConfigurationValue: 1).format(),
-	  zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId).format() ]
+	delayBetween([ zwave.configurationV1.configurationSet(parameterNumber: 250, scaledConfigurationValue: 1).format(),
+   // zwave.associationV1.associationSet(groupingIdentifier: 2, nodeId: zwaveHubNodeId).format(),
+   // zwave.associationV1.associationSet(groupingIdentifier: 3, nodeId: zwaveHubNodeId).format(),
+  //  zwave.associationV1.associationSet(groupingIdentifier: 4, nodeId: zwaveHubNodeId).format(),
+   // zwave.associationV1.associationSet(groupingIdentifier: 5, nodeId: zwaveHubNodeId).format(),
+    zwave.configurationV1.configurationSet(configurationValue: [1], parameterNumber: 3, size: 1).format(),
+    zwave.configurationV1.configurationSet(configurationValue: [3], parameterNumber: 8, size: 1).format(),
+	  zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId).format() ],500)
 }
 
 def configure() {
 	def cmd = configurationCmds()
-	log.debug("Sending configuration: $cmd")
+	//log.debug("Sending configuration: $cmd")
 	return cmd
 }
 
@@ -130,5 +170,6 @@ def updated() {
 }
 
 def initialize() {
-	sendEvent(name: "numberOfButtons", value: 4)
+	sendEvent(name: "numberOfButtons", value: 8)
+    log.debug("Sent numberOfButtons 8")
 }
